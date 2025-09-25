f0574a1056b5a23e96e96509a43b6fb03609137b
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-25 18:19:32 +0330

    feat: initial commit for src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Dữ Liệu Hành Vi Người Dùng</h1>
+      <p>Phân tích và trực quan hóa dữ liệu hành vi người dùng.</p>
+    </header>
+  );
+}
+
+export default Header;
